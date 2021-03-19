83653475edf24878a0b2da78e16d90a1a1624b20
eriudo <erildojunior@live.com>
2021-03-19 20:21:15 -0300

    test voucher

--- tests/test_service.py
+++ tests/test_service.py
@@ -17,34 +17,53 @@ def client():
     c = app.test_client()
     return c
 
-voucher1 = {
-    '_id': 1,
-    'type': "shipping",
-    'code': "#FRETEGRATIS",
-    'amount': 0,
-    'available': True
-}
-
 def test_get_product(client):
     c = client
 
     product1 = {
-        'id': 1,
-        'name': "Apple",
+        '_id': 1,
+        'name': 'Apple',
         'price': 10.3,
         'available': 10
     }
 
     product2 = {
-        'id': 1,
-        'name': "Banana",
+        '_id': 1,
+        'name': 'Banana',
         'price': 10.3,
         'available': 10
     }
 
+def test_get_voucher(client):
+    c = client
+
+    voucher1 = {
+        '_id': 1,
+        'type': 'shipping',
+        'code': '#FRETEGRATIS',
+        'amount': 0,
+        'available': True
+    }
+
+    voucher2 = {
+        'id': 2,
+        'type': 'percentual',
+        'code': '#30OFF',
+        'amount': 30,
+        'available': False
+    }
+
     response1 = c.post('/products', json=product1)
     response2 = c.post('/products', json=product2)
+    response3 = c.post('/vouchers', json=product1)
+    response4 = c.post('/vouchers', json=product2)
 
     json_data = response1.get_json()
+    json_data = response2.get_json()
+    json_data = response3.get_json()
+    json_data = response4.get_json()
 
     assert response1.status_code == 201
+    assert response2.status_code == 201
+    assert response3.status_code == 201
+    assert response4.status_code == 201
