016fcc27f86924b45e8fc62ea7af35c606e28b5c
Ítalo Vieira <italogomesv@gmail.com>
2021-03-20 10:46:11 -0300

    Register user routes

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -13,6 +13,11 @@ def configure_routes(app):
 
     api.add_resource(ProductRoute, '/products/<id>')
     api.add_resource(ProductListRoute, '/products')
+
     api.add_resource(VoucherRoute, '/vouchers/<id>')
     api.add_resource(VoucherListRoute, '/vouchers')
+
+    api.add_resource(UserRoute, '/users/<id>')
+    api.add_resource(UserListRoute, '/users')
+
     api.init_app(app)
