555f0a19aeb73de4e1254cb0546b185f38b211b0
eriudo <erildojunior@live.com>
2021-03-21 23:15:10 -0300

    test-service added Orders

--- tests/test_service.py
+++ tests/test_service.py
@@ -86,15 +86,74 @@ def test_get_voucher():
     saved_voucher1 = c.get('/vouchers/' + id_voucher1).get_json()
     saved_voucher2 = c.get('/vouchers/' + id_voucher2).get_json()
 
-     #Testando através da rota se o primeiro voucher foi cadastrado corretamente no banco
+    #Testando através da rota se o primeiro voucher foi cadastrado corretamente no banco
     assert saved_voucher1['type'] == 'shipping'
     assert saved_voucher1['code'] == '#FRETEGRATIS'
     assert saved_voucher1['amount'] == 0
     assert saved_voucher1['available'] == True
 
-     #Testando através da rota se o segundo voucher foi cadastrado corretamente no banco
+    #Testando através da rota se o segundo voucher foi cadastrado corretamente no banco
     assert saved_voucher2['type'] == 'percentual'
     assert saved_voucher2['code'] == '#30OFF'
     assert saved_voucher2['amount'] == 30
     assert saved_voucher2['available'] == False
 
+
+def test_get_order():
+    user = {
+        'name': 'italo',
+        'email': 'italo@ufrn.edu.br',
+        'password': '123456789'
+    }
+
+    response_user = c.post('/users', json=user)
+
+    id_user = response_user.get_json()
+
+    saved_user = c.get('/users/' + id_user).get_json()
+
+    product = {
+        'name': 'Pineaple',
+        'price': 6.3,
+        'available': 20
+    }
+
+    response_product = c.post('/products', json=product)
+
+    id_product = response_product.get_json()
+
+    saved_product = c.get('/products/' + id_product).get_json()
+
+    voucher = {
+        'type': 'percentual',
+        'code': '#30OFF',
+        'amount': 30,
+        'available': True
+    }
+   
+    response_voucher = c.post('/vouchers', json=voucher)
+ 
+    id_voucher = response_voucher.get_json()
+
+    saved_voucher = c.get('/vouchers/' + id_voucher).get_json()
+ 
+    order = {
+        'products:': [{'product_id': id_product, 'amount': 5}],
+        'voucher_id': id_voucher,
+        'date': '21/03/2021'
+    }
+
+    response_order = c.post('/users/' + id_user + '/orders', json=order)
+
+    id_order = response_order.get_json()
+
+    assert response_order.status_code == 201
+
+    saved_order = c.get('/users/' + id_user + '/orders').get_json()
+
+    first_product = saved_order['products'][0]
+
+    assert first_product['product_id'] == id_product
+    assert first_product['products'][0]['amount'] == 5
+    assert saved_order['voucher_id'] == id_voucher
+    assert saved_order['date'] == '21/03/2021'
