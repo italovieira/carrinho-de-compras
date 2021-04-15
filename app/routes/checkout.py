8535561ee59f2cc939db75df9c34070820e038c6
Ítalo Vieira <italogomesv@gmail.com>
2021-04-14 23:47:34 -0300

    Finish checkout route

---
+++ app/routes/checkout.py
@@ -0,0 +1,50 @@
+from flask_restful import Resource, reqparse
+from bson.objectid import ObjectId
+from ..dao.user import UserDAO
+from ..dao.product import ProductDAO
+from ..dao.voucher import VoucherDAO
+
+user_dao = UserDAO()
+product_dao = ProductDAO()
+voucher_dao = VoucherDAO()
+
+parser = reqparse.RequestParser()
+
+class CheckoutRoute(Resource):
+    def post(self, user_id, order_id):
+        order_dict = user_dao.get_order(user_id, order_id)
+
+        # Subtotal
+        subtotal = 0
+        quantity = 0
+        for item in order_dict['products']:
+            product = product_dao.get(item['product_id'])
+            subtotal += product['price'] * item['amount']
+            quantity += item['amount']
+
+
+        # Shipping
+        shipping = 0
+        if subtotal >= 400:
+            shipping = 0
+        elif quantity <= 10:
+            shipping = 30
+        else:
+            shipping = 50
+
+
+        # Voucher
+        voucher = voucher_dao.get(order_dict['voucher_id'])
+
+        total = 0
+        discount = 0
+        if voucher['type'] == 'percentual':
+            discount = subtotal * voucher['amount'] / 100
+        elif voucher['type'] == 'fixed':
+            discount = voucher['amount']
+        elif voucher['type'] == 'shipping':
+            shipping = 0
+
+        total = subtotal + shipping - discount
+
+        return { 'subtotal': subtotal, 'discount': discount, 'shipping': shipping, 'total': total }, 201
